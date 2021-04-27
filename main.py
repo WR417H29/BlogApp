a27d5f3048f51945eeb129d9b27439f0409b8dff
Wraith <i.acnaylor@gmail.com>
2021-04-27 10:02:53 +0100

    Decided to remove React App, and create the front end myself

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
-from flask import Flask
+from flask import Flask, request, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin
+from flask_login import LoginManager, UserMixin, login_required, login_user, current_user, logout_user
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
-app.config['SECRET_KEY'] = 'secretKey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fshuppprcznqku:c40eea0c5cc185e4cc9eb252ff51fb571f04b373cc612420338962638ceda414@ec2-54-155-35-88.eu-west-1.compute.amazonaws.com:5432/d325f9dtt49eu3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'whatKey'
 
 db = SQLAlchemy(app)
 loginManager = LoginManager()
+loginManager.init_app(app)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(30), unique=True, nullable=False)
-    password = db.Column(db.String(30), unique=False, nullable=False)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+
+    def __repr__(self):
+        return self.username
+
+@loginManager.user_loader
+def userLoader(userID):
+    return User.query.get(int(userID))
+
+@app.route('/')
+def index():
+    return render_template('index.html', title="Test", content="Test", link="login")
+
+@app.route('/login')
+def login():
+    user = User.query.filter_by(username="Admin").first()
+    login_user(user)
+    return render_template('index.html', title="Login", content="Logged In", link="home")
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
+
+@app.route('/home')
+@login_required
+def home():
+    return render_template('index.html', 
+        title="Home", 
+        content=f"Logged in as {current_user}", 
+        link="logout"
+        )
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
