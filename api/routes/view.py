eeb91b3b66369002f5063864fd5f9298f9d6cf5d
Wraith <i.acnaylor@gmail.com>
2021-05-18 12:14:15 +0100

    Added Profiles

--- api/routes/view.py
+++ api/routes/view.py
@@ -1,4 +1,4 @@
-from flask import request, render_template, url_for, redirect
+from flask import request, render_template, url_for, redirect, flash
 from flask_login import current_user, login_required
 
 from api import app, db
@@ -22,3 +22,15 @@ def home():
         replies = Reply.query.order_by(Reply.created_date).all()
 
         return render_template('view/index.html', posts=posts, User=User, replies=replies)
+
+
+@app.route('/profile/<string:_username>', methods=['GET', 'POST'])
+def profile_view(_username):
+    if request.method == 'GET':
+        user = User.query.filter_by(username=_username).first()
+        if not user:
+            flash("User doesn't exist, please try again")
+            return redirect(url_for('home'))
+
+        posts = Post.query.filter_by(author_id=user.id).all()
+        return render_template('view/profile.html', posts=posts, user=user, User=User)
